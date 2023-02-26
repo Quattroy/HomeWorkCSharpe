f613f5226aac8dc60470ea24cfb281b74cdec7d7
Igor Isupov <igor-i.s1@ya.ru>
2023-02-26 20:57:19 +0300

    выполнил задание 8

---
+++ task8/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число : ");
+int Number = int.Parse(Console.ReadLine() ?? " ");
+int x = 1;
+while (x <= Number)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine(x);
+    }
+        x++;
+    }
