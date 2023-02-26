1ac09e67514aa3ad8713314785f9a40aa72c9516
Igor Isupov <igor-i.s1@ya.ru>
2023-02-26 19:02:09 +0300

    Выполнил задание №2

---
+++ task 2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число №1: ");
+double firstNumber = Convert.ToDouble(Console.ReadLine ());
+Console.Write("Введите число №2: ");
+double secondNumber = Convert.ToDouble(Console.ReadLine ());
+if(firstNumber > secondNumber)
+    Console.Write("max = "+ firstNumber);
+else if(secondNumber > firstNumber)
+    Console.Write("max = "+ secondNumber);
+else Console.Write("числа равны");
