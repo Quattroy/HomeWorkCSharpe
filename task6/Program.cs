2db598b880cf1f53df28225679c70a43a6b53568
Igor Isupov <igor-i.s1@ya.ru>
2023-02-26 19:33:47 +0300

    Выполнено task6

---
+++ task6/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число : ");
+int Number = int.Parse(Console.ReadLine() ?? ""); 
+if(Number%2 == 0)
+    Console.Write("число четное");
+else Console.Write("число нечетное");
+
