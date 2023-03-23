6c5b52d2078396d52c65b2131e547099da4f133a
Igor Isupov <igor-i.s1@ya.ru>
2023-03-24 01:53:51 +0300

    Добмашнее задание по семинару №8

---
+++ task62_2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 62: Заполните спирально массив 4 на 4.
+// 1 2 3 4
+// 12 13 14 5
+// 11 16 15 6
+// 10 9 8 7
+
+Console.Clear();
+
+int n = 4;
+int[,] matrix = new int[n, n];
+
+int num = 1;
+int i = 0;
+int j = 0;
+
+while (num <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+  matrix[i, j] = num;
+  num++;
+  if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+    j++;
+  else if (i < j && i + j >= matrix.GetLength(0) - 1)
+    i++;
+  else if (i >= j && i + j > matrix.GetLength(1) - 1)
+    j--;
+  else
+    i--;
+}
+
+WriteArray(matrix);
+
+void WriteArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($" {array[i,j]}\t ");
+    }
+    Console.WriteLine();
+  }
+}
---
+++ task_62_spiral/Program.cs
@@ -0,0 +1,94 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+Console.Clear();
+int n = 4;
+int[,] array = new int[n, n];
+GetResult(array);
+
+void GetResult(int[,] array)
+{
+    int i = array.GetLength(0);
+    int j = array.GetLength(1);
+    int[,] arr = new int[i, j];
+    if(i == 0)
+    {
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+    if(i > 0 && j == 3)
+    {
+       for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+    if(i == 3 && j < 3)
+    {
+       for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+    if(i > 0 && i < 3 && j == 0)
+    {
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+    if(i == 1 && j > 0 && j < 3)
+    {
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+    if(i == 2 && j == 2)
+    {
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+    if(i == 2 && j == 1)
+    {
+        for (int k = 0; k < array.GetLength(0); k++)
+        {
+            for (int l = 0; l < array.GetLength(1); l++)
+            {
+                arr[k, l] = new Random().Next(-100, 100);
+                Console.Write($"{arr[i, j]}  ");
+            }
+        }
+    }
+}
